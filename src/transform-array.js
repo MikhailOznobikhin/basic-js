33312c19b52d7fa63b4a8ae4499ca0920cc98615
Mishgen1 <32348690+Mishgen1@users.noreply.github.com>
2020-10-01 00:51:00 +0800

    feat: not full transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!arr|| !Array.isArray(arr)) throw Error
+  let ot = []
+  if(arr.length === 0) return ot
+  
+  for(let i=0; i<arr.length; i++){
+    if(arr[i] === '--discard-next' && !!arr[i+2]){
+      i=i+2;  
+    }
+    if(arr[i] === '--discard-prev'){
+      ot.splice(i-1,i-1)
+    }
+    if(arr[i] === '--double-next' && !!arr[i+1]){
+      ot.push(arr[i+1])
+    }
+    if(arr[i] === '--double-prev' && !!arr[i-1]){
+        if(ot[ot.length-1] === arr[i-1]){
+          ot.push(ot[ot.length-1])
+        }
+    }
+  if(arr[i] !== '--double-prev' && arr[i] !== '--double-next' &&
+   arr[i] !== '--discard-prev' && arr[i] !== '--discard-next' && arr[i]
+    ){
+      ot.push(arr[i])
+    }
+   
+  }
+  return ot;
 };
