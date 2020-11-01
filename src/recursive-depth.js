714cd2fbf09d1635cb29699ad9d06ab607a94d27
Mishgen1 <32348690+Mishgen1@users.noreply.github.com>
2020-11-01 23:46:32 +0800

    feat: recurseve-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,20 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
- 
-  calculateDepth( arr ) {
-    Array.prototype.max = function() {
-      return Math.max.apply(null, this);
-    };
+  constructor(arr){
+    this.arr = arr;
+  }
 
-  function f1(item, level = 0){ 
-      if (item instanceof Array){
-          return item.map(function(value, index){
-              return f1(value, level + 1)
-          }).max() - (level > 0 ? 0 : 1) // Если главный массив не считаем
-      }
-      return level;
-  }
-  f1(arr)
-  }
+  calculateDepth(arr) {
+    var self = this;
+
+    return Array.isArray(arr) ? 1 + arr.reduce(function(a, b) {
+      return Math.max(a, self.calculateDepth(b));
+    }, 0) : 0;
+  };
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -27,5 +27,6 @@ module.exports = function transform(arr) {
     }
    
   }
+  
   return ot;
 };
